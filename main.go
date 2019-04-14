ce36af3e7e7f057723ff31c3a94807c78c6ab46a
assafmo <assaf.morami@gmail.com>
2019-04-14 20:42:54 +0300

    init args & open db

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+var queryStmt *sql.Stmt
+
+var dbPath string
+var queryString string
+var port uint
+
+var help = `SQLiteQueryServer
+
+`
+
+func init() {
+	flag.StringVar(&dbPath, "db", "", "Path to DB")
+	flag.StringVar(&queryString, "query", "", "The SQL query")
+	flag.UintVar(&port, "port", 80, "Port of the http server")
+
+	flag.Parse()
+
+	var err error
+	db, err = sql.Open("sqlite3", fmt.Sprintf("file:%s&mode=rw&cache=shared", dbPath))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryStmt, err = db.Prepare(queryString)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	http.HandleFunc("/query", query)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func query(w http.ResponseWriter, r *http.Request) {
 
 }
