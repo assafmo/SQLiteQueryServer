35a32c53efd53b4985fa020727349eecd102cf5d
assafmo <assaf.morami@gmail.com>
2019-04-17 12:54:32 +0300

    works

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -91,17 +91,27 @@ func query(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	outpoutEncoder := json.NewEncoder(w)
+	fmt.Fprintf(w, "[")
 
-	reqReader := csv.NewReader(bufio.NewReader(r.Body))
+	reqCsvReader := csv.NewReader(r.Body)
+	reqCsvReader.ReuseRecord = true
+
+	isFirstQuery := true
 	for {
-		csvLine, err := reqReader.Read()
-		if err == io.EOF {
+		csvLine, err := reqCsvReader.Read()
+		if err == io.EOF || err == http.ErrBodyReadAfterClose /*last line without \n*/ {
 			break
 		} else if err != nil {
 			http.Error(w,
-				fmt.Sprintf("Error reading request body: %v\n\n%s", err, helpMessege), http.StatusInternalServerError)
+				fmt.Sprintf("\n\nError reading request body: %v\n\n%s", err, helpMessege), http.StatusInternalServerError)
 			return
 		}
+
+		if !isFirstQuery {
+			fmt.Fprintf(w, ",")
+		}
+		isFirstQuery = false
 
 		queryParams := make([]interface{}, len(csvLine))
 		for i := range csvLine {
@@ -111,28 +121,56 @@ func query(w http.ResponseWriter, r *http.Request) {
 		rows, err := queryStmt.Query(queryParams...)
 		if err != nil {
 			http.Error(w,
-				fmt.Sprintf("Error executing query for params %v: %v\n\n%s", csvLine, err, helpMessege), http.StatusInternalServerError)
+				fmt.Sprintf("\n\nError executing query for params %v: %v\n\n%s", csvLine, err, helpMessege), http.StatusInternalServerError)
 			return
 		}
 		defer rows.Close()
 
+		cols, err := rows.Columns()
+		if err != nil {
+			http.Error(w,
+				fmt.Sprintf("\n\nError executing query for params %v: %v\n\n%s", csvLine, err, helpMessege), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, `{"in":`)
+		outpoutEncoder.Encode(csvLine)
+		fmt.Fprintf(w, ",")
+		fmt.Fprintf(w, `"headers":`)
+		outpoutEncoder.Encode(cols)
+		fmt.Fprintf(w, `,"out":[`)
+
+		isFirstRow := true
 		for rows.Next() {
-			var row []interface{}
-			err = rows.Scan(&row)
+			if !isFirstRow {
+				fmt.Fprintf(w, ",")
+			}
+			isFirstRow = false
+
+			row := make([]interface{}, len(cols))
+			pointers := make([]interface{}, len(row))
+
+			for i := range row {
+				pointers[i] = &row[i]
+			}
+
+			err = rows.Scan(pointers...)
 			if err != nil {
 				http.Error(w,
-					fmt.Sprintf("Error reading query results for params %v: %v\n\n%s", csvLine, err, helpMessege), http.StatusInternalServerError)
+					fmt.Sprintf("\n\nError reading query results for params %v: %v\n\n%s", csvLine, err, helpMessege), http.StatusInternalServerError)
 				return
 			}
 
-			fmt.Println(row)
-			// TODO print row to w (as part of a json [{"in":csvLine, "out":[[],[],...,[]]}])
+			outpoutEncoder.Encode(row)
 		}
 		err = rows.Err()
 		if err != nil {
 			http.Error(w,
-				fmt.Sprintf("Error executing query: %v\n\n%s", err, helpMessege), http.StatusInternalServerError)
+				fmt.Sprintf("\n\nError executing query: %v\n\n%s", err, helpMessege), http.StatusInternalServerError)
 			return
 		}
+
+		fmt.Fprintf(w, "]}")
 	}
+	fmt.Fprintf(w, "]")
 }
