bdf162589fbfa4789fe46dd3664f5badeaa1e259
assafmo <assaf.morami@gmail.com>
2019-04-18 13:58:25 +0300

    build script also zip

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# build into ./build/
+rm -rf build
+mkdir -p build
+
+VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+go get -u -v github.com/karalabe/xgo
+
+xgo --targets windows/amd64 --dest build --out SQLiteQueryServer-"${VERSION}" .
+xgo --targets linux/amd64   --dest build --out SQLiteQueryServer-"${VERSION}" --tags linux --ldflags "-extldflags -static"  .
+
+(
+    cd build
+    find -type f | 
+    parallel --bar 'zip "$(echo "{}" | sed "s/.exe//").zip" "{}"'
+)
