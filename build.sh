308b250fe0e3165445bc4d617e04850829e7378d
assafmo <assaf.morami@gmail.com>
2019-04-28 16:48:47 +0300

    change build directory from "build" to "release"

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# build into ./build/
+# build into ./release/
 
 set -e
 set -v
 
 go test -race ./...
 
-rm -rf build
-mkdir -p build
+rm -rf release
+mkdir -p release
 
 VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 go get -u -v github.com/karalabe/xgo
 
-xgo --targets windows/amd64 --dest build --out SQLiteQueryServer-"${VERSION}" .
-xgo --targets linux/amd64   --dest build --out SQLiteQueryServer-"${VERSION}" --tags linux --ldflags "-extldflags -static"  .
+xgo --targets windows/amd64 --dest release --out SQLiteQueryServer-"${VERSION}" .
+xgo --targets linux/amd64   --dest release --out SQLiteQueryServer-"${VERSION}" --tags linux --ldflags "-extldflags -static"  .
 
 (
-    cd build
+    cd release
     find -type f | 
     parallel --bar 'zip "$(echo "{}" | sed "s/.exe//").zip" "{}"'
 )
