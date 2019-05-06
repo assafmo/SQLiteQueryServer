c5001a11824f12d62e2ccef32b1b9749c6d4c827
assafmo <assaf.morami@gmail.com>
2019-05-06 09:30:17 +0300

    DEBIAN/control Installed-Size, Provides, Conflicts, Replaces

--- build.sh
+++ build.sh
@@ -26,20 +26,24 @@ xgo --targets linux/amd64   --dest release --out SQLiteQueryServer-"${VERSION}" 
         parallel --bar 'zip "$(echo "{}" | sed "s/.exe//").zip" "{}" && rm -f "{}"'
 
     # deb
-    mkdir -p ./deb/DEBIAN
-    cat > ./deb/DEBIAN/control <<EOF 
-Package: SQLiteQueryServer
-Architecture: amd64
-Maintainer: Assaf Morami <assaf.morami@gmail.com>
-Priority: optional
-Version: $(echo "${VERSION}" | tr -d v)
-Homepage: https://github.com/assafmo/SQLiteQueryServer
-Description: Bulk query SQLite database over the network. 
-EOF
-
     mkdir -p ./deb/bin
     unzip -o -d ./deb/bin *-linux-amd64.zip
     mv -f ./deb/bin/*-linux-amd64 ./deb/bin/SQLiteQueryServer
+
+    mkdir -p ./deb/DEBIAN
+    cat > ./deb/DEBIAN/control <<EOF 
+Package: SQLiteQueryServer
+Version: $(echo "${VERSION}" | tr -d v)
+Priority: optional
+Architecture: amd64
+Maintainer: Assaf Morami <assaf.morami@gmail.com>
+Homepage: https://github.com/assafmo/SQLiteQueryServer
+Installed-Size: $(stat --printf="%s" ./deb/bin/SQLiteQueryServer)
+Provides: sqlitequeryserver
+Conflicts: sqlitequeryserver
+Replaces: sqlitequeryserver
+Description: Bulk query SQLite database over the network. 
+EOF
 
     dpkg-deb --build ./deb/ .
 )
