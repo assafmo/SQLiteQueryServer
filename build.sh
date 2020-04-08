1be3530ef4963e5acdfa1e4b50de9166e2af1f5a
assafmo <assaf.morami@gmail.com>
2020-04-08 16:24:52 +0300

    also compile to macos

--- build.sh
+++ build.sh
@@ -17,7 +17,8 @@ VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 go get -u -v github.com/karalabe/xgo
 
 xgo --targets windows/amd64 --dest release --out SQLiteQueryServer-"${VERSION}" .
-xgo --targets linux/amd64   --dest release --out SQLiteQueryServer-"${VERSION}" --tags linux --ldflags "-extldflags -static"  .
+xgo --targets linux/amd64   --dest release --out SQLiteQueryServer-"${VERSION}" --tags linux --ldflags "-extldflags -static" .
+xgo --targets darwin/amd64  --dest release --out SQLiteQueryServer-"${VERSION}" --tags darwin .
 
 (
     # zip
